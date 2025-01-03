9fa4ab96a7cd2c520a6662bb03b7d831fb2fdaae
kevinzhao2337 <zhao.chu1@northeastern.edu>
2025-01-02 23:38:43 -0800

    CommitFizzBuzz

---
+++ 01FizzBuzz/01FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                if (i > 100)
+                {
+                    break;
+                }
+                else if (i % (3 * 5) == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
